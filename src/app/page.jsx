9e4bdc4b530f31a111cb6fff524d4de3a175087e
Hidehumi Takahashi <hidehumi.takahashi@partner.ibm.com>
2023-10-11 14:24:53 +0900

    add homeheader

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,12 @@
 import React from "react";
+import HomeHeader from "./comonents/HomeHeader";
 // https://www.youtube.com/watch?v=h9zlVmoUV5I&t=20436s
+//35:00～
 
 export default function Home() {
-  return <div>那実</div>;
+  return (
+    <>
+      <HomeHeader />
+    </>
+  );
 }
